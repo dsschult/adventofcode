5c45da1f4b9c8d42bacfb48572b09385c5927403
David Schultz <davids24@gmail.com>
2024-12-05 23:53:08 -0600

    day 6

---
+++ 2024/day6_2/src/main.rs
@@ -0,0 +1,171 @@
+use std::fs::read_to_string;
+
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename) 
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(String::from)  // make each slice into a string
+        .collect()  // gather them together into a vector
+}
+
+#[derive(PartialEq, Debug, Clone)]
+enum Facing {
+    Up,
+    Right,
+    Left,
+    Down
+}
+
+impl Facing {
+    fn next_facing(&self) -> Facing {
+        match self {
+            Facing::Up => Facing::Right,
+            Facing::Right => Facing::Down,
+            Facing::Down => Facing::Left,
+            Facing::Left => Facing::Up,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Map {
+    objects: Vec<Vec<bool>>,
+    guard: (i32, i32)
+}
+
+impl Map {
+    fn new(lines: &Vec<String>) -> Map {
+        let mut ret = Vec::new();
+        let mut guard = None;
+
+        for line in lines.iter() {
+            let mut row = Vec::new();
+            if line.trim().is_empty() {
+                continue;
+            }
+            for c in line.chars() {
+                match c {
+                    '^' => {
+                        guard = Some((ret.len() as i32, row.len() as i32));
+                        row.push(false);
+                    },
+                    '#' => {
+                        row.push(true);
+                    },
+                    _ => {
+                        row.push(false);
+                    }
+                };
+            }
+            ret.push(row);
+        }
+        Map{ objects: ret, guard: guard.expect("no guard!") }
+    }
+
+    fn add_obstruction(&self, x: usize, y: usize) -> Option<Map> {
+        if self.objects[x][y] {
+            return None
+        }
+        if self.guard == (x as i32, y as i32) {
+            return None
+        }
+        let mut m = self.clone();
+        m.objects[x][y] = true;
+        Some(m)
+    }
+
+    fn walk_guard(&self) -> Option<usize> {
+        let mut guard_pos = Vec::new();
+        let rows = self.objects.len();
+        let cols = self.objects[0].len();
+        for _ in 0..rows {
+            let mut row = Vec::new();
+            for _ in 0..cols {
+                row.push(None);
+            }
+            guard_pos.push(row);
+        }
+        let mut facing = Facing::Up;
+        guard_pos[self.guard.0 as usize][self.guard.1 as usize] = Some(facing.clone());
+        let mut current_pos = self.guard.clone();
+
+        loop {
+            let next_pos = match facing {
+                Facing::Up => (current_pos.0-1, current_pos.1),
+                Facing::Right => (current_pos.0, current_pos.1+1),
+                Facing::Down => (current_pos.0+1, current_pos.1),
+                Facing::Left => (current_pos.0, current_pos.1-1)
+            };
+            if next_pos.0 < 0 || next_pos.0 >= rows as i32 || next_pos.1 < 0 || next_pos.1 >= cols as i32 {
+                // guard is out!
+                break;
+            }
+            if self.objects[next_pos.0 as usize][next_pos.1 as usize] {
+                println!("hit object at {:?}", next_pos);
+                facing = facing.next_facing();
+            } else if guard_pos[next_pos.0 as usize][next_pos.1 as usize] == Some(facing.clone()) {
+                println!("loop at {:?}", next_pos);
+                return None;
+            } else {
+                guard_pos[next_pos.0 as usize][next_pos.1 as usize] = Some(facing.clone());
+                current_pos = next_pos;
+            }
+        }
+        for row in guard_pos.iter() {
+            println!("{}", row.iter().map(|x| match x { Some(_) => 'X', _ => '.'}).collect::<String>());
+        }
+
+        Some(guard_pos.into_iter().fold(0, |r, col| r + col.into_iter().filter(|x| x.is_some()).count()))
+    }
+
+    fn find_loops(&self) -> usize {
+        let mut ret = 0;
+        let rows = self.objects.len();
+        let cols = self.objects[0].len();
+        for i in 0..rows {
+            for j in 0..cols {
+                match self.add_obstruction(i,j) {
+                    Some(m) => {
+                        if m.walk_guard() == None {
+                            ret += 1;
+                        }
+                    },
+                    None => { }
+                };
+            }
+        }
+        ret
+    }
+}
+
+fn main() {
+    let lines = read_lines("input");
+    let map = Map::new(&lines);
+    println!("Guard: {:?}", map.find_loops());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let sample: Vec<String> = "
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+".lines().map(String::from).collect();
+
+        let map = Map::new(&sample);
+        assert_eq!(map.walk_guard(), Some(41));
+
+        assert_eq!(map.find_loops(), 6);
+    }
+}
